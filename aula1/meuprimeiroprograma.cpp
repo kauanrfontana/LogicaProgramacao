74990f12632ccd4e8cbfce02a8c25c2ee6c65f90
kauanrfontana <kauanrfontana@gmail.com>
2023-04-28 20:13:27 -0300

    adicionados comentários para as linhas

--- aula1/meuprimeiroprograma.cpp
+++ aula1/meuprimeiroprograma.cpp
@@ -1,9 +1,23 @@
+/* #include <iostream> é uma biblioteca de arquivos de
+cabeçalho que nos permite trabalhar com objetos de entrada e saída,
+como cout (usado na linha 5). Os arquivos de cabeçalho adicionam
+funcionalidade aos programas C ++.*/
 #include <iostream>
+
+// using namespace std significa que podemos usar nomes para objetos e variáveis da biblioteca padrão.
 using namespace std;
+
+// int main() função padrão para executar código
 int main() {
     setlocale(LC_ALL, "portuguese");
+
+    /* cout é um objeto usado junto com
+    o operador de inserção ( <<) para produzir / imprimir texto. Em nosso
+    exemplo, a saída será "Olá mundo".*/
     cout << "Ola mundo! \n";
     cout << "Ola mundo2!" << endl;
     cout << "Ola mundo3! \n";
+
+    // finaliza a execução da função
     return 0;
 }
