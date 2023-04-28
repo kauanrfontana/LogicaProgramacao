5d284d289f2f7fcd7cba9f841d54e148ae6686ff
kauanrfontana <kauanrfontana@gmail.com>
2023-04-28 20:05:36 -0300

    adicionado primeiro arquivo de aula1

---
+++ aula1/meuprimeiroprograma.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    setlocale(LC_ALL, "portuguese");
+    cout << "Ola mundo! \n";
+    cout << "Ola mundo2!" << endl;
+    cout << "Ola mundo3! \n";
+    return 0;
+}
