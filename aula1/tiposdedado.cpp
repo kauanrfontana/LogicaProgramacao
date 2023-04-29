d3d2b5b1ead3efb316d031befe75520efd4a4c2f
kauanrfontana <kauanrfontana@gmail.com>
2023-04-28 21:07:14 -0300

    adicionado arquivo sobre tipos de dados em C++

---
+++ aula1/tiposdedado.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    setlocale(LC_ALL, "portuguese");
+
+    int inteiro = 1;
+    double decimal = 1.2;
+    string texto = "texto";
+    char caracter = 'A';
+    bool booleano = true;
+
+    cout << "Váriavel inteira: " << inteiro << endl;
+    cout << "Váriavel decimal: " << decimal << endl;
+    cout << "Váriavel caracter: " << caracter << endl;
+    cout << "Váriavel inteira: " << inteiro << endl;
+    cout << "Váriavel booleano: " << booleano << endl;
+
+
+    return 0;
+}
